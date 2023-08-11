0b2b5cff2086256efed3f9e51c74c0e3d1cd7838
Henrique <henriquebzn1@gmail.com>
2023-08-11 03:17:00 -0300

    Worked on header text

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,8 +2,13 @@ import React, { FC } from 'react'
 
 const Header: FC = () => {
   return (
-    <header className="relative h-screen flex items-center justify-center">
-      Header
+    <header className="relative h-screen flex flex-col items-center justify-center text-white text-center">
+      <h1 className="leading-loose -mt-[11%]">
+        Websites that are <br />
+        <span className="text-7xl font-bold tracking-tight">
+          OUT OF THIS WORLD
+        </span>
+      </h1>
     </header>
   )
 }
