50e106e0073125640c71f3c48303eac619f20879
Henrique <henriquebzn1@gmail.com>
2023-08-11 04:38:54 -0300

    Added 3 main planet images

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,14 +1,22 @@
+import PlanetWithRing from '@/public/PlanetWithRing'
+import Sun from '@/public/Sun'
 import React, { FC } from 'react'
 
 const Header: FC = () => {
   return (
-    <header className="relative h-screen flex flex-col items-center justify-center text-white text-center">
-      <h1 className="leading-loose -mt-[11%]">
-        Websites that are <br />
-        <span className="text-7xl font-bold tracking-tight">
-          OUT OF THIS WORLD
-        </span>
-      </h1>
+    <header className="h-screen flex items-center justify-center text-white text-center ">
+      <div className="relative flex flex-col items-center -mt-[12%] ">
+        <Sun className="w-24 mb-3 " />
+
+        <h1 className="leading-loose ">
+          Websites that are <br />
+          <span className="text-7xl font-black tracking-tight">
+            OUT OF THIS WORLD
+          </span>
+        </h1>
+
+        <PlanetWithRing className="absolute -left-64 top-64 w-60 rotate-[-325deg]" />
+      </div>{' '}
     </header>
   )
 }
--- app/Services.tsx
+++ app/Services.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Services = () => {
   return (
     <section className="relative flex items-center justify-center max-w-[1250px] w-screen bg-white border-4 border-dark-purple rounded-3xl  mt-[-70px] h-44">
-      <GreenPlanet className="absolute -top-64 -right-36 z-50 w-80" />
+      <GreenPlanet className="absolute -top-60 -right-36 z-50 w-72" />
     </section>
   )
 }
