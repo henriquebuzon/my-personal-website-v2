9e2ab7da9af0fd18c72c34b011c1779d4b185b6b
Henrique <henriquebzn1@gmail.com>
2023-08-11 10:28:26 -0300

    Added meteor image on services section

--- app/Services.tsx
+++ app/Services.tsx
@@ -1,9 +1,11 @@
 import GreenPlanet from '@/public/GreenPlanet'
+import MeteorOnfire from '@/public/MeteorOnfire'
 import React from 'react'
 
 const Services = () => {
   return (
-    <section className="relative flex items-center justify-center max-w-[1250px] w-screen bg-white border-4 border-dark-purple rounded-3xl  mt-[-70px] h-44 ">
+    <section className="relative flex items-center justify-center max-w-[1250px] w-screen bg-white border-4 border-dark-purple rounded-3xl  mt-[-70px] h-56 ">
+      <MeteorOnfire className="absolute -bottom-32 -left-36 z-50 w-72 rotate-[54deg] " />
       <GreenPlanet className="absolute -top-60 -right-36 z-50 w-72 -rotate-45 " />
     </section>
   )
