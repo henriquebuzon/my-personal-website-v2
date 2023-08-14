40cd4855d7e14363e99b02bbef2f19c7d5bbe473
Henrique <henriquebzn1@gmail.com>
2023-08-14 16:00:23 -0300

    Finished sending email on form submission

--- app/Contact.tsx
+++ app/Contact.tsx
@@ -1,11 +1,43 @@
+'use client'
+
 import EmphasisContact from '@/public/EmphasisContact'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import ImagesContactForm from './ImagesContactForm'
+import emailjs from '@emailjs/browser'
 
 const Contact: FC = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendEmail = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (!name || !email || !message) {
+      alert('Please fill all inputs before submitting the email.')
+      return
+    }
+
+    await emailjs.send(
+      'service_5i0u279',
+      'template_04f6mso',
+      {
+        from_name: name,
+        from_email: email,
+        message,
+      },
+      'Aj7BwPtLnrdxj28qd'
+    )
+
+    setEmail('')
+    setMessage('')
+    setName('')
+    alert('Email sent!')
+  }
+
   return (
     <section className="bg-[#f1f1f1] w-full flex justify-center border-b-4 border-dark-purple">
-      <div className="relative w-full max-w-[1250px] flex flex-col justify-center items-center space-y-4 pb-48 pt-36 mx-3 lg:mx-10">
+      <div className="relative w-full max-w-[1250px] flex flex-col justify-center items-center space-y-4 pb-48 pt-32 mx-3 lg:mx-10">
         <ImagesContactForm />
 
         <div className="flex flex-col space-y-6 items-center">
@@ -17,26 +49,36 @@ const Contact: FC = () => {
           <p>Make contact</p>
         </div>
 
-        <form className="w-full max-w-lg flex flex-col space-y-3">
+        <form
+          className="w-full max-w-lg flex flex-col space-y-3"
+          onSubmit={sendEmail}
+        >
           <input
             type="text"
             className="bg-transparent py-4 rounded-full border-dark-purple border-[3px] mt-5 px-8 w-full"
             placeholder="Your name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
           <input
             type="text"
             className="bg-transparent py-4 rounded-full border-dark-purple border-[3px] mt-5 px-8 w-full"
             placeholder="Your email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
           <textarea
             rows={6}
             className="bg-transparent py-4 rounded-[2.2rem] border-dark-purple border-[3px] mt-5 px-8 w-full"
             placeholder="Your message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
           />
-
-          <button className="py-4 bg-pink rounded-full border-dark-purple border-[3px] font-bold w-full">
-            Send
-          </button>
+          <input
+            type="submit"
+            className="py-4 bg-pink rounded-full border-dark-purple border-[3px] font-bold w-full cursor-pointer"
+            value="Send"
+          />
         </form>
       </div>
     </section>
--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,36 +4,36 @@ import React, { FC } from 'react'
 const Footer: FC = () => {
   return (
     <footer className=" w-full flex flex-col justify-center items-center  text-white">
-      <div className="relative mt-[22rem] mb-44 flex justify-center items-center px-3 lg:px-10 ">
+      <div className="relative mt-[21.2rem] mb-44 flex justify-center items-center px-3 lg:px-10 ">
         <h2 className="font-extrabold text-3xl lg:text-5xl ">
           Lets <span className="text-pink">solve</span> this puzzle
         </h2>
-        <Galaxy className="absolute w-52 -z-10 opacity-20 " />
+        <Galaxy className="absolute w-52 -z-10 opacity-10 " />
       </div>
 
       <div className="w-full z-50  flex flex-col md:flex-row items-center justify-center text-dark-purple text-sm gap-5">
-        <div className="flex flex-col md:flex-row justify-between items-center w-screen max-w-[1250px]  bg-white  rounded-t-3xl p-10 border-t-4 border-x-4 border-dark-purple gap-5 md:gap-0">
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center w-screen max-w-[1250px]  bg-white  rounded-t-3xl p-10 border-t-4 border-x-4 border-dark-purple gap-5 md:gap-0">
           <p className="opacity-80">
             © Copyright 2021 by{' '}
             <span className="font-semibold">Henrique Buzon</span>
           </p>
           <div className="flex flex-row font-semibold gap-5">
             <a
-              href="https://www.linkedin.com/in/your-profile/"
+              href="https://www.linkedin.com/in/henriquebuzon/"
               target="_blank"
               rel="noopener noreferrer"
             >
               LinkedIn
             </a>
             <a
-              href="https://www.linkedin.com/in/your-profile/"
+              href="https://www.github.com/in/henriquebuzon/"
               target="_blank"
               rel="noopener noreferrer"
             >
               GitHub
             </a>
             <a
-              href="https://www.linkedin.com/in/your-profile/"
+              href="https://www.linkedin.com/in/henrique/"
               target="_blank"
               rel="noopener noreferrer"
             >
